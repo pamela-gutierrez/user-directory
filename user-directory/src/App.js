25de1c4cc632b385d1dfe450882e6ec6ddd0309d
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-02-24 23:20:05 -0800

    add table jsx

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -2,16 +2,27 @@ import React from "react";
 // import Home from "./pages/Home";
 import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
-import Container from "./components/Container";
+// import Container from "./components/Container";
 import TableRows from "./components/TableRows";
+import TableHead from "./components/TableHead";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
-      <Container />
       <SearchForm />
-      <TableRows />
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        <TableRows />
+      </table>
     </div>
   );
 }
--- user-directory/src/components/TableRows/index.js
+++ user-directory/src/components/TableRows/index.js
@@ -2,36 +2,29 @@ import React from "react";
 
 function TableRows() {
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
     )
 }
 
