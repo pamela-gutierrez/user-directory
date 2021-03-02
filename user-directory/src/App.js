8cb9455a39a5f9bd91b4d1b4ab3dcc9f39a7d869
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-03-01 20:23:57 -0800

    update filter and add search function

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -10,7 +10,10 @@ import API from "./utils/API.js";
 
 function App() {
 
-  const [employees, setEmployees] = useState([]);
+  const [employees, setEmployees] = useState({ originalArr: [], filteredArr: [] });
+  // const [search, setSearch] = useState("");
+  // const [direction, setDirection] = useState("");
+  // const [col, setCol] = useState()
 
   useEffect(() => {
     loadEmployees();
@@ -18,32 +21,61 @@ function App() {
 
   function loadEmployees() {
     API.getEmployees().then((employees) => {
-      setEmployees(employees.sort())
+      console.log(employees)
+      setEmployees({ ...employees, originalArr: employees, filteredArr: employees })
     })
   }
+
+  function handleSearch(event) {
+    var searchEmployees = event.target.value
+    const filteredArr = employees.originalArr.filter(employee => employee.firstname.indexOf(event.target.value) !== -1)
+    setEmployees({ ...employees, filteredArr: filteredArr })
+  }
+
+  function sortEmployeesByName() {
+    const sortEmployees = employees.originalArr.sort((a, b) => {
+      const firstEmp = a.firstname.toUpperCase();
+      const nextEmp = b.firstname.toUpperCase();
+      if (firstEmp < nextEmp) return -1;
+      else if (firstEmp > nextEmp) return 1;
+      return 0;
+    });
+    console.log(sortEmployees)
+    setEmployees({ ...employees, filteredArr: sortEmployees });
+  }
+
+
 
   function capitalizeFirstLetter(string = "") {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  // if (!employees) {
+  //   return (
+  //     <p>Please wait.</p>
+  //   )
+  // } else {
+  //   console.log(employees)
+
+
   return (
     <div>
       <Header />
-      <SearchForm />
+      <SearchForm handleSearch={(event) => handleSearch(event)} />
       <table className="table table-striped">
         {/* <th scope="row"></th> */}
         {/* <TableHead /> */}
         <thead>
           <tr>
             {/* <th scope="col">Image</th> */}
-            <th scope="col">Name</th>
+            <th scope="col" onClick={() => sortEmployeesByName()}>Name</th>
             <th scope="col">Phone</th>
             <th scope="col">Email</th>
             <th scope="col">DOB</th>
           </tr>
         </thead>
         <tbody>
-          {employees.map((employee, index) => {
+          {employees.filteredArr.map((employee, index) => {
             return (
               <TableRows
                 name={capitalizeFirstLetter(employee.firstname) + " " + capitalizeFirstLetter(employee.lastname)}
@@ -52,12 +84,15 @@ function App() {
                 // image={employee.picture.thumbnail}
                 dob={employee.dob}
               />
+              // <div><p>{employee.firstname}</p></div>
             )
           })}
         </tbody>
       </table>
+
     </div>
   );
 }
+
 
 export default App;
--- user-directory/src/components/SearchForm/index.js
+++ user-directory/src/components/SearchForm/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.css";
 
-function SearchForm() {
+function SearchForm(props) {
     return (
         <form>
             <div className="mb-3">
                 <label for="exampleInputEmail1" className="form-label"></label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search" />
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search" onChange={props.handleSearch} />
             </div>
         </form>
 
